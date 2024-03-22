43390a4784ae235c857d7f5d6257c2d4db6d0665
nikosa <svde@outlook.com>
2024-03-22 16:56:36 +0000

    init

---
+++ epdg/epdg-pinger.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if the file containing FQDNs is provided as an argument
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <file_containing_fqdns>"
+    exit 1
+fi
+
+# Check if the file exists
+if [ ! -f "$1" ]; then
+    echo "Error: File '$1' not found."
+    exit 1
+fi
+
+# Read FQDNs from the file and execute ping for each of them
+while IFS= read -r fqdn; do
+    echo "Pinging $fqdn ..."
+    ping -c 1 -W 0.3 "$fqdn" | grep  epdg |grep bytes
+    #echo "--------------------------------------"
+done < "$1"
